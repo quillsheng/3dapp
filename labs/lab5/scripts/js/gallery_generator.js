bbfcc77d9a0c6515326cb9c8bc2d54b03ad352a1
Quill Sheng <qs55@sussex.ac.uk>
2022-04-19 17:17:54 +0100

    Lab5 gallery part

---
+++ labs/lab5/scripts/js/gallery_generator.js
@@ -0,0 +1,32 @@
+// JavaScript Gallery Generator
+$(document).ready(function() {
+	// Create the XMLHttpRequest Object for communicating with the web server
+	var xmlHttp = new XMLHttpRequest ();
+    // Stores newly generated gallery HTML code
+	var htmlCode = "";
+	// Temporarily stores server response while code is generated
+	var response;
+	// Set up the path to the PHP function that reads the image directory to find the thumbnail file names
+	var send = "scripts/php/hook.php";
+	// Open the connection to the web server
+	xmlHttp.open("GET", send, true);
+	// Tell the server that the client has no outgoing message, i.e. we are sending nothing to the server
+	xmlHttp.send(null);
+	// Check we get a valid server response
+	xmlHttp.onreadystatechange = function() {
+		if(xmlHttp.readyState == 4) {
+			// Response handler code
+			//alert(xmlHttp.responseText);
+			response = xmlHttp.responseText.split("~");
+
+			for (var i=0;i<response.length;i++) {
+				htmlCode += '<a href="assets/images/gallery_images' + response[i] +'">';
+				htmlCode += '<img class="card-img-top img-thumbnail" src="assets/images/gallery_images' + response[i] + '"/>';
+				htmlCode += '</a>';
+			}
+			document.getElementById('gallery_coke').innerHTML = htmlCode;
+			document.getElementById('gallery_sprite').innerHTML = htmlCode;
+			document.getElementById('gallery_pepper').innerHTML = htmlCode;		
+		}
+	}
+});
